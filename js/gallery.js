98717dcc585fa1dae08714299da276775417cf95
Alexis Rossfelder <contact.liteapplication@gmail.com>
2024-07-23 15:12:30 +0200

    Change header level

--- js/gallery.js
+++ js/gallery.js
@@ -218,11 +218,10 @@ function hint_slide(gallery) {
 
 function markdownToHtml(markdown) {
   // Convert headers
-  markdown = markdown.replace(/^##### (.*$)/gim, "<h6>$1</h6>");
-  markdown = markdown.replace(/^#### (.*$)/gim, "<h5>$1</h5>");
-  markdown = markdown.replace(/^### (.*$)/gim, "<h4>$1</h4>");
-  markdown = markdown.replace(/^## (.*$)/gim, "<h3>$1</h3>");
-  markdown = markdown.replace(/^# (.*$)/gim, "<h2>$1</h2>");
+  markdown = markdown.replace(/^#### (.*$)/gim, "<h6>$1</h6>");
+  markdown = markdown.replace(/^### (.*$)/gim, "<h5>$1</h5>");
+  markdown = markdown.replace(/^## (.*$)/gim, "<h4>$1</h4>");
+  markdown = markdown.replace(/^# (.*$)/gim, "<h3>$1</h3>");
 
   // Convert bold text
   markdown = markdown.replace(/\*\*(.*)\*\*/gim, "<strong>$1</strong>");
